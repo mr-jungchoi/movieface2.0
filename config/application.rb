d70bb609c149413ad42831b06e92c11c5f257ed4
“Sofia <sofiaclara93@gmail.com>
2016-12-12 16:58:23 -0500

    add require statements

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
+require 'open-uri'
+require 'json'
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
