8e09858e0abded5bf8bc989b9b1596188da14257
“Sofia <sofiaclara93@gmail.com>
2016-12-12 16:45:58 -0500

    add movie routes:

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,36 @@
 class MoviesController < ApplicationController
+  def index
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    search_keyword = params[:search]
+    youtube_response = open(youtube_url + search_keyword + trailer_search(search_keyword) + youtube_token).read
+    omdb_response = open(omdb_url + search_keyword).read
+    @movie = Movie.parse(youtube_response, omdb_response)
+    @movie.save
+  end
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :youtube_trailer_id, :imdb_id)
+  end
+
+  def youtube_url
+    "https://www.googleapis.com/youtube/v3/search?part=snippet&q="
+  end
+
+  def omdb_url
+    "http://www.omdbapi.com/?y=&plot=short&r=json&t="
+  end
+
+  def trailer_search(movie_name)
+    "#{movie_name}+trailer"
+  end
 end
