0bbef4633c46be4f96f463f22e2b6f0aaa588249
jdollete <[01;31m[K"email"[m[K: "allain.dollete@gmail.com>
2016-12-12 17:48:30 -0500

    Correct login parameters

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_username(params[:session][:username])
-
+    user = User.find_by_email(params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       redirect_to user_path(user)
