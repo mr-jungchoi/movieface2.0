8a949ba8ec3992a6307388760511bade898c0042
lorelei522 <lorelei522@gmail.com>
2016-12-13 17:11:56 -0500

    fix the favorites controller, need another end. Fix indents

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,22 +3,23 @@ class FavoritesController < ApplicationController
 
   def new
     if logged_in?
-     @favorite = Favorite.new
-   else
-     redirect_to new_session_path
-   end
+      @favorite = Favorite.new
+    else
+      redirect_to new_session_path
+    end
   end
 
   def create
     if logged_in?
       @favorite = @movie.favorites.create
       @favorite.user_id = current_user.id
-      if @favorite.save
-        redirect_to movie_path(@movie)
+    end  
+    if @favorite.save
+      redirect_to movie_path(@movie)
     else
-    redirect_to new_session_path
+      redirect_to new_session_path
+    end
   end
-end
 
   private
     def find_movie
