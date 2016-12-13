ddc1bec5ec92a12650ab899dada4b3db91255911
“Sofia <sofiaclara93@gmail.com>
2016-12-13 17:59:01 -0500

    add carousel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,11 +17,5 @@
 //= require components
 //= require_tree .
 $(document).ready(function(){
-     $('.carousel').carousel({
-           dist:0,
-           shift:0,
-           padding:20,
-
-     });
-
-   });
+    $('.carousel').carousel();
+  });
