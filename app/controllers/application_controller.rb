d9d0bc741e2581a1ab67f6871a7f75aa6959de46
“Sofia <sofiaclara93@gmail.com>
2016-12-13 12:04:34 -0500

    protect routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user, :logged_in?, :capitalize_search,
+  helper_method :current_user, :logged_in?, :movie_reviewed?, :movie_favorited?
 
   def youtube_token
     @youtube_token ||= "&key=#{Dotenv.load["YOUTUBE_TOKEN"]}"
@@ -11,6 +11,26 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find_by_id(session[:user_id])
+  end
+
+  def movie_reviewed?
+    @current_user.reviews.each do |review|
+      if @movie.id == review.movie_id
+        return true
+      else
+        return false
+      end
+    end
+  end
+
+  def movie_favorited?
+    @current_user.favorites.each do |favorite|
+      if @movie.id == favorite.movie_id
+        return true
+      else
+        return false
+      end
+    end
   end
 
   protect_from_forgery with: :exception
