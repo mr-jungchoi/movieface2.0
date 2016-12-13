8bf31dc32bc23089b84ebfaaea1509506faecf14
Asiya Gizatulina <asya.asha@gmail.com>
2016-12-13 11:03:16 -0500

    favorites

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
 
   resources :movies, except: [:edit, :update] do
     resources :reviews
+    resources :favorites, except: [ :edit, :update]
   end
   resources :genres, only: [:index, :show]
 
   resources :sessions, except: [:index, :show, :edit, :update]
   resources :users
 
+
   root :to => 'genres#index'
+
 end
