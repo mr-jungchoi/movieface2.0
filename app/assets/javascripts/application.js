2aab3e3b660e4c3218310b28a1ae0f823aed05f2
Sofia Cepeda <sofiaclara93@gmail.com>
2016-12-13 18:19:44 -0500

    Update application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,21 +17,5 @@
 //= require components
 //= require_tree .
 $(document).ready(function(){
-<<<<<<< HEAD
-<<<<<<< HEAD
     $('.carousel').carousel();
   });
-=======
-     $('.carousel').carousel({
-           dist:0,
-           shift:0,
-           padding:20,
-
-     });
-
-   });
->>>>>>> fix issues
-=======
-    $('.carousel').carousel();
-  });
->>>>>>> add carousel
