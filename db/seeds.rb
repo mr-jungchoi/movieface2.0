914d1a78c99893431667d3c8322e42a2b1f88ae7
mr-jungchoi <mr.jungchoi@gmail.com>
2016-12-13 16:46:56 -0500

    Refactor seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,14 @@ f.each_line do |line|
 end
 
 movies.each do |title|
-  omdb_response = open("http://www.omdbapi.com/?y=&plot=short&r=json&t=#{title}").read
-  youtube_response = open("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=#{title}+trailer&key=#{Dotenv.load["YOUTUBE_TOKEN"]}").read
-  movie = Movie.parse(youtube_response, omdb_response)
-  movie_genre = Genre.parse(omdb_response)
-  genre = Genre.find_or_create_by(name: movie_genre.name)
-  movie.genre = genre
-  movie.save
+  movie = Movie.find_by_title(title)
+  if movie == nil
+    omdb_response = open("http://www.omdbapi.com/?y=&plot=short&r=json&t=#{title}").read
+    youtube_response = open("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=#{title}+trailer&key=#{Dotenv.load["YOUTUBE_TOKEN"]}").read
+    movie = Movie.parse(youtube_response, omdb_response)
+    movie_genre = Genre.parse(omdb_response)
+    genre = Genre.find_or_create_by(name: movie_genre.name)
+    movie.genre = genre
+    movie.save
+  end
 end
