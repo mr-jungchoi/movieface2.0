3f5f7b074b9272a2ceeaa1464709cc37c984aa5c
mr-jungchoi <mr.jungchoi@gmail.com>
2017-01-04 14:31:57 -0500

    Fix typo in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ movies.each do |title|
 
   omdb_response = open("http://www.omdbapi.com/?y=&plot=short&r=json&t=#{title}").read
   youtube_response = open("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=#{title}+trailer&key=#{ENV["YOUTUBE_TOKEN"]}").read
-  print youtube_reponse
+  print youtube_response
   movie = Movie.parse(youtube_response, omdb_response)
   movie_genre = Genre.parse(omdb_response)
   genre = Genre.find_or_create_by(name: movie_genre.name)
