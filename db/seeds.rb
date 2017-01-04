1ff28bbd3656b685f01c94f3132a819b8c9ce3d5
mr-jungchoi <mr.jungchoi@gmail.com>
2017-01-04 14:25:39 -0500

    Add debugger to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,13 @@ end
 
 movies.each do |title|
   movie = Movie.find_by_title(title)
-  
-  if movie == nil
-    omdb_response = open("http://www.omdbapi.com/?y=&plot=short&r=json&t=#{title}").read
-    youtube_response = open("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=#{title}+trailer&key=#{Dotenv.load["YOUTUBE_TOKEN"]}").read
-    movie = Movie.parse(youtube_response, omdb_response)
-    movie_genre = Genre.parse(omdb_response)
-    genre = Genre.find_or_create_by(name: movie_genre.name)
-    movie.genre = genre
-    movie.save
-  end
+
+  omdb_response = open("http://www.omdbapi.com/?y=&plot=short&r=json&t=#{title}").read
+  youtube_response = open("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=#{title}+trailer&key=#{ENV["YOUTUBE_TOKEN"]}").read
+  print youtube_reponse
+  movie = Movie.parse(youtube_response, omdb_response)
+  movie_genre = Genre.parse(omdb_response)
+  genre = Genre.find_or_create_by(name: movie_genre.name)
+  movie.genre = genre
+  movie.save
 end
